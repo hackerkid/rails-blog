134795b0f9b3fa87ce8b77bea5d53b43f082c732
Vishnu Ks <hackerkid@vishnuks.com>
2016-01-26 23:14:19 +0530

    till deletion implemented

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,35 @@
 class ArticlesController < ApplicationController
+	
+
 	def new
-	end
+    @article = Article.new
+  end
+ 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
+  def create
+    @article = Article.new(article_params)
 
-	def create
-  		@article = Article.new(article_params)
- 		@article.save
-  		redirect_to @article
-	end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+	
+  def show
+   	@article = Article.find(params[:id])
+  end
 
-	 def show
-   	 	@article = Article.find(params[:id])
-  	 end
-
-  	 def index
-  	 	@articles = Article.all
-  	 end
+  def index
+  	@articles = Article.all
+  end
  
 	private
-  		def article_params
-    		params.require(:article).permit(:title, :text)
-  		end
+  	def article_params
+    	params.require(:article).permit(:title, :text)
+  	end
 
 end
